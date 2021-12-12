4665960744008f15a6aef9160be3ecdfd0a2a221
VladislavBakaev <bakaev.98@bk.ru>
2021-12-12 15:01:22 +0300

    ff

---
+++ build/phantomx_control/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sim/phantom_x/src/phantomx_control
+# Build directory: /home/sim/phantom_x/build/phantomx_control
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/phantomx_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sim/phantom_x/src/phantomx_gazebo
+# Build directory: /home/sim/phantom_x/build/phantomx_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
